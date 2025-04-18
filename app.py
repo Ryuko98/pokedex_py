3acc13b34d0eec26c9fcc7af55b950407702f32b
ryukomvp <daniel123hernandez15@gmail.com>
2025-04-18 03:46:21 -0600

    Buscar regiones. Falta pulir pero ya funciona

--- app.py
+++ app.py
@@ -1,18 +1,24 @@
 from flask import Flask, render_template, request
-from controllers.pokemon_controller import find_pokemon
+from controllers.pokemon_controller import find_pokemon, find_region
 
 app = Flask(__name__)
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
     pokemon = None
+    region = None
     error = None
 
     if request.method == 'POST':
-        nombre = request.form.get('pokemon_name')
-        pokemon, error = find_pokemon(nombre)
+        search_type = request.form.get('search_type')
+        query = request.form.get('query')
+        
+        if search_type == 'pokemon':
+            pokemon, error = find_pokemon(query)
+        elif search_type == 'region':
+            region, error = find_region(query)
 
-    return render_template('index.html', pokemon=pokemon, error=error)
+    return render_template('index.html', pokemon=pokemon, region=region, error=error)
 
 if __name__ == '__main__':
     app.run(debug=True)
