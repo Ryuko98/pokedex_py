61864abf3ab67940d6c2c3833b5bfdf5a9a26010
ryukomvp <daniel123hernandez15@gmail.com>
2025-04-17 22:49:58 -0600

    Mejora para obtener el nombre de los tipos y habilidades. Se obtienen directamente desde la api y no se corrigen con un diccionario

--- models/pokeapi.py
+++ models/pokeapi.py
@@ -7,3 +7,23 @@ def get_pokemon(name):
     if resp.status_code == 200:
         return resp.json()
     return None
+
+def get_type_name(tipo):
+    url = f"https://pokeapi.co/api/v2/type/{tipo}/"
+    resp = requests.get(url)
+    if resp.status_code == 200:
+        data = resp.json()
+        for name in data['names']:
+            if name['language']['name'] == 'es':
+                return name['name']
+    return None
+
+def get_ability_name(ability):
+    url = f"https://pokeapi.co/api/v2/ability/{ability}/"
+    resp = requests.get(url)
+    if resp.status_code == 200:
+        data = resp.json()
+        for name in data['names']:
+            if name['language']['name'] == 'es':
+                return name['name']
+    return None
--- models/pokemon_model.py
+++ models/pokemon_model.py
@@ -1,4 +1,4 @@
-from models.pokeapi import get_pokemon
+from models.pokeapi import get_pokemon, get_type_name, get_ability_name
 
 def get_pokemon_data(name):
     data = get_pokemon(name)
@@ -11,55 +11,6 @@ def get_pokemon_data(name):
         'height': data['height'],
         'weight': data['weight'],
         'sprites': data['sprites']['front_default'],
-        'types': [types_dict.get(t['type']['name'], t['type']['name'].capitalize()) for t in data['types']],
-        'abilities': [abilities_dict.get(a['ability']['name'], a['ability']['name'].capitalize()) for a in data['abilities']]
+        'types': [get_type_name(t['type']['name']) for t in data['types']],
+        'abilities': [get_ability_name(a['ability']['name']) for a in data['abilities']]
     }
-
-types_dict = {
-    'fire': 'Fuego',
-    'water': 'Agua',
-    'grass': 'Planta',
-    'electric': 'Eléctrico',
-    'dragon': 'Dragón',
-    'ground': 'Tierra',
-    'fighting': 'Lucha',
-    'psychic': 'Psíquico',
-    'ghost': 'Fantasma',
-    'dark': 'Siniestro',
-    'fairy': 'Hada',
-    'ice': 'Hielo',
-    'rock': 'Roca',
-    'steel': 'Acero',
-    'bug': 'Bicho',
-    'normal': 'Normal',
-    'poison': 'Veneno',
-    'flying': 'Volador'
-}
-
-abilities_dict = {
-    'overgrow': 'Espesura',
-    'blaze': 'Mar llamas',
-    'torrent': 'Torrente',
-    'shield-dust': 'Polvo escudo',
-    'shed-skin': 'Muda',
-    'compound-eyes': 'Ojocompuesto',
-    'intimidate': 'Intimidación',
-    'static': 'Electricidad estática',
-    'levitate': 'Levitación',
-    'pressure': 'Presión',
-    'synchronize': 'Sincronía',
-    'chlorophyll': 'Clorofila',
-    'cute-charm': 'Gran encanto',
-    'run-away': 'Fuga',
-    'keen-eye': 'Vista lince',
-    'sand-veil': 'Velo arena',
-    'rough-skin': 'Piel tosca',
-    'flash-fire': 'Absorbe fuego',
-    'swift-swim': 'Nado rápido',
-    'poison-point': 'Punto tóxico',
-    'inner-focus': 'Foco interno',
-    'guts': 'Agallas',
-    'adaptability': 'Adaptable',
-    'technician': 'Técnico',
-    'levitate': 'Levitación'
-}
