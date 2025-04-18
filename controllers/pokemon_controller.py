32ceda3eb8d26e7c994f58675b5ea64ac034dfd2
ryukomvp <daniel123hernandez15@gmail.com>
2025-04-18 02:26:09 -0600

    Más validaciones para el nombre de la especie solicitada

--- controllers/pokemon_controller.py
+++ controllers/pokemon_controller.py
@@ -1,9 +1,10 @@
 from models.pokemon_model import get_pokemon_data
 
 def find_pokemon(name):
-    if not name:
+    if not name or not name.strip():
         return None, "Debes ingresar un nombre o índice de Pokémon."
 
+    name = name.strip().lower()
     pokemon = get_pokemon_data(name)
     if pokemon:
         return pokemon, None
